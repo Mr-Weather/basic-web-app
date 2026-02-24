faa5b22b870771006bb1f4bb008631b01cc00c70
冯俊开 <junka1@junka1air-4.wifi.local.cmu.edu>
2026-02-24 10:09:28 -0500

    plus-of-two

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -22,5 +22,12 @@ export default function QueryProcessor(query: string): string {
     }
   }
 
+  if (query.toLowerCase().includes("plus")) {
+    const nums = query.match(/-?\d+/g)?.map(Number) ?? [];
+    if (nums.length > 0) {
+      return String(nums.reduce((a, b) => a + b, 0));
+    }
+  }
+
   return "";
 }
